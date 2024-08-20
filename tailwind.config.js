edc4cb0abe709f6be5e831789160944196750250
Dnyaneshwarkangude <dnyaneshwarkangude@gmail.com>
2024-08-20 12:21:57 +0530

    update login and register component

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
   ],
   darkMode:"class",
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'customShadow': '5px 5px 11px #bebebe, -5px -5px 11px #ffffff',
+        'customShadow1' : '6px 6px 10px #949494, -6px -6px 10px #ffffff',
+      },
+    },
   },
   plugins: [],
 }
